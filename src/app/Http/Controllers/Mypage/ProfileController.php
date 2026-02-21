ac491be2bcdc61ea9370abfab5b875ed82c3b2d1
nakagawa yuki <mstyk1021@gmail.com>
2026-02-21 10:02:08 +0900

    プロフィール画面

--- src/app/Http/Controllers/Mypage/ProfileController.php
+++ src/app/Http/Controllers/Mypage/ProfileController.php
@@ -9,6 +9,6 @@ class ProfileController extends Controller
 {
     public function edit()
     {
-        return view('mypage.index');
+        return view('mypage.profile');
     }
 }
---
+++ src/resources/views/mypage/profile.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/profile.css') }}" >
+@endsection
+
+@section('content')
+<div class="mypage-profile__content">
+    <div class="mypage-profile__heading">
+        <h2>プロフィール設定</h2>
+    </div>
+    <form class="form" action="/mypage/profile" method="POST">
+        @csrf
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">ユーザー名</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="name" value="{{ old('name') }}"/>
+                </div>
+                <div class="form__error">
+
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">郵便番号</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="postal_code" value="{{ old('postal_code') }}"/>
+                </div>
+                <div class="form__error">
+
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">住所</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="address" value="{{ old('address') }}"/>
+                </div>
+                <div class="form__error">
+
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">建物名</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="building" value="{{ old('building') }}"/>
+                </div>
+                <div class="form__error">
+
+                </div>
+            </div>
+        </div>
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">更新する</button>
+        </div>
+    </form>
+</div>
+@endsection
